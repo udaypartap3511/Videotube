138a797b98ebff001eb16c20e71eaa1f804c5e15
udaypartap3511 <upartap12@gmail.com>
2026-09-18 13:26:54 +0530

    add healthchecker

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ import subscriptionRouter from './routes/subscription.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import likeRouter from './routes/like.routes.js'
 import commentRouter from './routes/comment.routes.js'
+import  healthcheckRouter  from './routes/healthcheck.routes.js'
 
 
 //routes declaration
@@ -37,7 +38,7 @@ app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/playlists",playlistRouter)
 app.use("/api/v1/likes",likeRouter)
 app.use('/api/v1/comments',commentRouter)
-
+app.use('/api/v1/healthchecks',healthcheckRouter)
 
 
 export {app}
